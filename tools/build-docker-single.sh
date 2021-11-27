5e27f2710f3b07bde6e2700cf8654949c5c553bd
Taiki Endo <te316e89@gmail.com>
2021-11-28 04:24:39 +0900

    Add qemu-user

---
+++ tools/build-docker-single.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cd "$(cd "$(dirname "$0")" && pwd)"/..
+
+if [[ $# -gt 1 ]]; then
+    cat <<EOF
+USAGE:
+    $0
+EOF
+    exit 1
+fi
+set -x
+package="$1"
+
+export DOCKER_BUILDKIT=1
+
+owner="${OWNER:-taiki-e}"
+platform="${PLATFORM:-"linux/amd64,linux/arm64/v8"}"
+base_tag="ghcr.io/${owner}/${package}"
+time="$(date --utc '+%Y-%m-%d-%H-%M-%S')"
+
+build() {
+    local dockerfile="${package}/Dockerfile"
+    local full_tag="${base_tag}:latest"
+    local build_args=(
+        --file "${dockerfile}" "${package}/"
+        --platform "${platform}"
+        --tag "${full_tag}"
+    )
+
+    if [[ -n "${PUSH_TO_GHCR:-}" ]]; then
+        docker buildx build --push "${build_args[@]}"
+        docker pull "${full_tag}"
+        docker history "${full_tag}"
+    elif [[ "${platform}" == *","* ]]; then
+        docker buildx build "${build_args[@]}"
+    else
+        docker buildx build --load "${build_args[@]}"
+        docker history "${full_tag}"
+    fi
+}
+
+log_dir="tmp/log/${package}"
+mkdir -p "${log_dir}"
+build 2>&1 | tee "${log_dir}/build-docker-${time}.log"

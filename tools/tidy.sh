88f7230f9ca9efc884a559d8de40679fc18c9053
Taiki Endo <te316e89@gmail.com>
2021-12-20 22:27:07 +0900

    Do not exit early in tidy in CI

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -16,12 +16,20 @@ cd "$(cd "$(dirname "$0")" && pwd)"/..
 x() {
     local cmd="$1"
     shift
-    (
-        set -x
+    if [[ -n "${verbose:-}" ]]; then
+        (
+            set -x
+            "$cmd" "$@"
+        )
+    else
         "$cmd" "$@"
-    )
+    fi
 }
 
+if [[ "${1:-}" == "-v" ]]; then
+    shift
+    verbose=1
+fi
 if [[ $# -gt 0 ]]; then
     cat <<EOF
 USAGE:
@@ -35,28 +43,20 @@ if type -P npm &>/dev/null && type -P "$(npm bin)/prettier" &>/dev/null; then
     prettier="$(npm bin)/prettier"
 fi
 
-if [[ -z "${CI:-}" ]]; then
-    if type -P shfmt &>/dev/null; then
-        x shfmt -l -w $(git ls-files '*.sh')
-    else
-        echo >&2 "WARNING: 'shfmt' is not installed"
-    fi
-    if type -P "${prettier}" &>/dev/null; then
-        x "${prettier}" -l -w $(git ls-files '*.yml')
-    else
-        echo >&2 "WARNING: 'prettier' is not installed"
-    fi
-    if type -P shellcheck &>/dev/null; then
-        x shellcheck $(git ls-files '*.sh')
-        # SC2154 doesn't seem to work on dockerfile.
-        x shellcheck -e SC2148,SC2154 $(git ls-files '*Dockerfile')
-    else
-        echo >&2 "WARNING: 'shellcheck' is not installed"
-    fi
+if type -P shfmt &>/dev/null; then
+    x shfmt -l -w $(git ls-files '*.sh')
 else
-    x shfmt -d $(git ls-files '*.sh')
-    x "${prettier}" -c $(git ls-files '*.yml')
+    echo >&2 "WARNING: 'shfmt' is not installed"
+fi
+if type -P "${prettier}" &>/dev/null; then
+    x "${prettier}" -l -w $(git ls-files '*.yml')
+else
+    echo >&2 "WARNING: 'prettier' is not installed"
+fi
+if type -P shellcheck &>/dev/null; then
     x shellcheck $(git ls-files '*.sh')
     # SC2154 doesn't seem to work on dockerfile.
     x shellcheck -e SC2148,SC2154 $(git ls-files '*Dockerfile')
+else
+    echo >&2 "WARNING: 'shellcheck' is not installed"
 fi

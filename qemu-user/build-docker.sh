903150b2e27819c2c82e8a1677c7790403c24d70
Taiki Endo <te316e89@gmail.com>
2023-11-18 12:47:23 +0900

    qemu-user: Add 8.2.0-rc0

--- qemu-user/build-docker.sh
+++ qemu-user/build-docker.sh
@@ -40,10 +40,12 @@ time=$(date -u '+%Y-%m-%d-%H-%M-%S')
 # https://tracker.debian.org/pkg/qemu
 latest=8.1
 versions=(
+    8.2
     8.1
     7.2
 )
 dpkg_versions=(
+    8.2.0~rc0+ds-1
     8.1.2+ds-1
     7.2+dfsg-7+deb12u2
 )

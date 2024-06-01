2c8c1b00cbdea5faa9d17e9ecb3b7bc41154ecfb
Taiki Endo <te316e89@gmail.com>
2024-06-01 10:36:21 +0900

    qemu-user: Update 7.2 to 7.2+dfsg-7+deb12u6, 8.2 to 8.2.4+ds-2

--- qemu-user/build-docker.sh
+++ qemu-user/build-docker.sh
@@ -41,8 +41,8 @@ time=$(date -u '+%Y-%m-%d-%H-%M-%S')
 latest=8.2
 dpkg_versions=(
     9.0.0~rc2+ds-1
-    8.2.4+ds-1
-    7.2+dfsg-7+deb12u5
+    8.2.4+ds-2
+    7.2+dfsg-7+deb12u6
 )
 
 build() {

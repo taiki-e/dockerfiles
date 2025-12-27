bf672566c37d96572a3aa2d157f9249abd5ccc3e
Taiki Endo <te316e89@gmail.com>
2025-12-27 19:42:16 +0900

    qemu-user: Update latest to 10.2, 10.2 to 10.2.0+ds-1

--- qemu-user/build-docker.sh
+++ qemu-user/build-docker.sh
@@ -24,9 +24,9 @@ platform="${PLATFORM:-"linux/amd64,linux/arm64/v8"}"
 
 # https://ftp.debian.org/debian/pool/main/q/qemu
 # https://tracker.debian.org/pkg/qemu
-latest=10.1
+latest=10.2
 dpkg_versions=(
-  10.2.0~rc1+ds-1
+  10.2.0+ds-1
   10.1.2+ds-3+b1
   10.0.6+ds-0+deb13u2
 )

de6dcf435c872f6334016c8b6e4ca099f7aea4c0
Taiki Endo <te316e89@gmail.com>
2026-03-22 16:07:53 +0900

    qemu-system: Remove

--- qemu-system/README.md
+++
@@ -1,4 +0,0 @@
-# qemu-system
-
-There is no stability guarantee for this image, since it's supposed to only be
-used in projects managed by us.
--- qemu-system/shared.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/false
-# SPDX-License-Identifier: Apache-2.0 OR MIT
-# shellcheck shell=bash # not executable
-# shellcheck disable=SC2034
-
-# https://www.qemu.org/download/
-version="${QEMU_VERSION:-10.2.1}"
-latest=10.2
-target="${QEMU_TARGET:-}"
